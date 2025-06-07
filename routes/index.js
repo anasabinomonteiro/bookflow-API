20ad17994959c397ebf63086ef466d180861cad6
limanjohnson <liamjnell@gmail.com>
2025-06-06 19:33:16 -0600

    update routes to include author http requests

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const books = require('../controllers/books');
+const authors = require('../controllers/authors');
 
 router.get('/', (req, res) => {
   res.send('Hello World!');
@@ -11,5 +12,12 @@ router.get('/:book_id', books.findOne);
 router.post('/', books.create);
 router.put('/:book_id', books.updateBook);
 router.delete('/:book_id', books.deleteBook);
+
+// Author Routes
+router.get('/', authors.findAll);
+router.get('/:author_id', authors.findOne);
+router.post('/', authors.create);
+router.put('/:author_id', authors.updateAuthor);
+router.delete('/:author_id', authors.deleteAuthor);
 
 module.exports = router;
